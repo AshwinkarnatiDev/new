7a61e894ffb41723787b4fcd0b6edcd2f8be4823
Ashwinkarnati <ashwinkarnati2k5@gmail.com>
2024-11-25 21:04:36 +0530

    filled register.py

--- register.py
+++ register.py
@@ -5,4 +5,8 @@ root = Tk()
 root.title="Registration"
 
 Label(text="Email").grid(row=1,column=0,padx=5,pady=5)
-Label(text="Password").
+Label(text="Password").grid(row=2,column=0,padx=5,pady=5)
+email_entry=Entry(root)
+pass_entry=Entry(root)
+
+root.mainloop()
